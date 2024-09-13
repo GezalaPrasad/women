49b9a7faa8e9a8a6ec436b88f620d25d9c0dc065
Ankush Gupta <guptankush1@gmail.com>
2024-09-13 16:28:57 +0530

    testimonials

--- index.php
+++ index.php
@@ -23,15 +23,18 @@ include 'components/wishlist_cart.php';
    <title>home</title>
 
    <link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css" />
-   
    <!-- font awesome cdn link  -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
-
+   <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="css/font-awesome.min.css" type="text/css">
+    <link rel="stylesheet" href="css/elegant-icons.css" type="text/css">
+    <link rel="stylesheet" href="css/nice-select.css" type="text/css">
+    <link rel="stylesheet" href="css/jquery-ui.min.css" type="text/css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css" type="text/css">
+    <link rel="stylesheet" href="css/slicknav.min.css" type="text/css">
    <!-- custom css file link  -->
    <link rel="stylesheet" href="css/style.css">
    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.css">
-
 </head>
 <body>
    
@@ -46,42 +49,97 @@ include 'components/wishlist_cart.php';
    <h1 class="quote">Protecting what matters</h1>
 <br><br><br><br><br><br><br><br><br><br><br><br>
 </section>
-
 </div>
-<div class="container rounded"> <div class="d-flex justify-content-center fas fa-heart"></div> <p class="tag">Our customers love</p> <h1 class="text-primary text-center head">What we do</h1> <div class="owl-carousel owl-theme"> <div class="owl-item"> <div class="card d-flex flex-column"> <div class="mt-2"> <span class="fas fa-star active-star"></span> <span class="fas fa-star active-star"></span> <span class="fas fa-star active-star"></span> <span class="fas fa-star active-star"></span> <span class="fas fa-star-half-alt active-star"></span> </div> <div class="main font-weight-bold pb-2 pt-1">Great Service</div> <div class="testimonial"> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni dolores molestias veniam inventore itaque eius iure omnis, temporibus culpa id. </div> <div class="d-flex flex-row profile pt-4 mt-auto"> <img src="https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="" class="rounded-circle"> <div class="d-flex flex-column pl-2"> <div class="name">Megan</div> <p class="text-muted designation">CEO of My Company</p> </div> </div> </div> </div> <div class="owl-item"> <div class="card d-flex flex-column"> <div class="mt-2"> <span class="fas fa-star active-star"></span> <span class="fas fa-star active-star"></span> <span class="fas fa-star active-star"></span> <span class="fas fa-star active-star"></span> <span class="fas fa-star-half-alt active-star"></span> </div> <div class="main font-weight-bold pb-2 pt-1">Great Service</div> <div class="testimonial"> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni dolores molestias veniam inventore itaque eius iure omnis, temporibus culpa id. </div> <div class="d-flex flex-row profile pt-4 mt-auto"> <img src="https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="" class="rounded-circle"> <div class="d-flex flex-column pl-2"> <div class="name">Megan</div> <p class="text-muted designation">CEO of My Company</p> </div> </div> </div> </div> <div class="owl-item"> <div class="card d-flex flex-column"> <div class="mt-2"> <span class="fas fa-star active-star"></span> <span class="fas fa-star active-star"></span> <span class="fas fa-star active-star"></span> <span class="fas fa-star active-star"></span> <span class="fas fa-star-half-alt active-star"></span> </div> <div class="main font-weight-bold pb-2 pt-1">Great Service</div> <div class="testimonial"> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni dolores molestias veniam inventore itaque eius iure omnis, temporibus culpa id. </div> <div class="d-flex flex-row profile pt-4 mt-auto"> <img src="https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="" class="rounded-circle"> <div class="d-flex flex-column pl-2"> <div class="name">Megan</div> <p class="text-muted designation">CEO of My Company</p> </div> </div> </div> </div> <div class="owl-item"> <div class="card d-flex flex-column"> <div class="mt-2"> <span class="fas fa-star active-star"></span> <span class="fas fa-star active-star"></span> <span class="fas fa-star active-star"></span> <span class="fas fa-star active-star"></span> <span class="fas fa-star-half-alt active-star"></span> </div> <div class="main font-weight-bold pb-2 pt-1">Great Service</div> <div class="testimonial"> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni dolores molestias veniam inventore itaque eius iure omnis, temporibus culpa id. </div> <div class="d-flex flex-row profile pt-4 mt-auto"> <img src="https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="" class="rounded-circle"> <div class="d-flex flex-column pl-2"> <div class="name">Megan</div> <p class="text-muted designation">CEO of My Company</p> </div> </div> </div> </div> <div class="owl-item"> <div class="card d-flex flex-column"> <div class="mt-2"> <span class="fas fa-star active-star"></span> <span class="fas fa-star active-star"></span> <span class="fas fa-star active-star"></span> <span class="fas fa-star active-star"></span> <span class="fas fa-star-half-alt active-star"></span> </div> <div class="main font-weight-bold pb-2 pt-1">Great Service</div> <div class="testimonial"> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni dolores molestias veniam inventore itaque eius iure omnis, temporibus culpa id. </div> <div class="d-flex flex-row profile pt-4 mt-auto"> <img src="https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="" class="rounded-circle"> <div class="d-flex flex-column pl-2"> <div class="name">Megan</div> <p class="text-muted designation">CEO of My Company</p> </div> </div> </div> </div> </div> </div>
+
+
+
+
+<section class="section testimonial-section">
+<div class="text-center mb-5">
+            <h2 class="testimonial-heading">What our community is saying</h2>
+        </div>
+    <div class="container py-5">
+        <!-- Heading -->
+        
+    
+        <!-- Main Review Section -->
+        <div class="row justify-content-center">
+            <!-- Left Review -->
+            <div class="col-lg-4 col-md-6 abcd">
+                <div class="testimonial-card p-4">
+                    <p>“I loved my stay here. The activities were plentiful, and the vibes were fantastic. The kitchen and living space had everything you need, plus the gym was a great amenity...”</p>
+                    <h6>Kiev B. <span class="rating">★ 4.5</span></h6>
+                </div>
+            </div>
+    
+            <!-- Featured Review -->
+            <div class="col-lg-4 col-md-6">
+                <div class="testimonial-card p-5 text-center featured-review">
+                    <h2 class="rating">4.9</h2>
+                    <p class="mb-1 rating-text">Excellent!</p>
+                    <p class="text-muted">Based on 33,000 reviews</p>
+                    <p>★★★★★</p>
+                </div>
+            </div>
+    
+            <!-- Right Review -->
+            <div class="col-lg-4 col-md-6">
+                <div class="testimonial-card p-4">
+                    <p>“I had an incredible time staying here. The place was peaceful and offered stunning views. There was a great sense of community, which I really appreciated...”</p>
+                    <h6>Daniel B. <span class="rating">★ 4.9</span></h6>
+                </div>
+            </div>
+        </div>
+    
+        <!-- Other Reviews Section -->
+        <div class="row">
+            <!-- Repeat this testimonial-card structure for each testimonial -->
+            <div class="col-lg-4 col-md-6">
+                <div class="testimonial-card p-4">
+                    <p>“This is an extremely beautiful, modern house with everything you need for your stay. Claire is a nice host. She is doing everything that your stay is a whole pleasure...”</p>
+                    <h6>Matthias B. <span class="rating">★ 5.0</span></h6>
+                </div>
+            </div>
+    
+            <div class="col-lg-4 col-md-6">
+                <div class="testimonial-card p-4">
+                    <p>“I absolutely loved my time here. The people are incredibly kind, and the owner and staff do everything they can to make sure everyone feels part of a special community...”</p>
+                    <h6>Angela G. <span class="rating">★ 4.6</span></h6>
+                </div>
+            </div>
+    
+            <div class="col-lg-4 col-md-6">
+                <div class="testimonial-card p-4">
+                    <p>“I stayed at this coliving space, and the views were absolutely incredible. It's definitely worth a visit if you're looking for a breathtaking scenery.”</p>
+                    <h6>Alex C. <span class="rating">★ 5.0</span></h6>
+                </div>
+            </div>
+    
+            <!-- Add more testimonial cards similarly as per requirement -->
+        </div>
+    </div>
+  </section>
+ <!-- Teatimonials ends -->
+
+
+
+
+
 
 <?php include 'components/footer.php'; ?>
-
+<script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.nice-select.min.js"></script>
+    <script src="js/jquery-ui.min.js"></script>
+    <script src="js/jquery.slicknav.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
 <script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
-
+<script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.2.1/owl.carousel.js"></script> 
 <script src="js/script.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.2.1/owl.carousel.js"></script> 
-<script> 
-$(document).ready(function () {
-    var silder = $(".owl-carousel");
-    silder.owlCarousel({
-        autoPlay: false,
-        items: 1,
-        center: false,
-        nav: true,
-        margin: 40,
-        dots: false,
-        loop: true,
-        navText: ["<i class='fa fa-arrow-left' aria-hidden='true'></i>", "<i class='fa fa-arrow-right' aria-hidden='true'></i>"],
-        responsive: {
-            0: {
-                items: 1,
-            },
-            575: { items: 1 },
-            768: { items: 2 },
-            991: { items: 3 },
-            1200: { items: 4 }
-        }
-    });
-});
-</script>
+
+
 </body>
 </html>
